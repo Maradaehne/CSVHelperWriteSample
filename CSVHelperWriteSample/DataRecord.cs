c0a1a3e470d33fb1569221cc726d35064bdf46da
André Dähne <a.daehne@gmx.de>
2017-11-08 01:07:55 +0100

    init log

---
+++ CSVHelperWriteSample/DataRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+
+namespace CSVHelperWriteSample
+{
+    class DataRecord
+    {
+        public String CommonName { get; set; }
+        public String FormalName { get; set; }
+        public String TelephoneCode { get; set; }
+        public String CountryCode { get; set; }
+    }
+
+    public class BeispielRecord
+    {
+        public String Spalte1 { get; set; }
+        public String Spalte2 { get; set; }
+        public String Spalte3 { get; set; }
+        public String Spalte4 { get; set; }
+    }
+
+    public sealed class BeispielRecordMap : ClassMap<BeispielRecord>
+    {
+        public BeispielRecordMap()
+        {
+            Map(m => m.Spalte1).Name("Spalte 1");
+            Map(m => m.Spalte2).Name("Spalte 2");
+            Map(m => m.Spalte3).Name("Spalte 3");
+            Map(m => m.Spalte3).Name("Spalte 4");
+        }
+    }
+
+}
---
+++ CSVHelperWriteSample/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace CSVHelperWriteSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var sr = new StreamReader(@"countrylist.csv"))
+            //{
+            //    using (var sw = new StreamWriter(@"countrylistoutput.csv"))
+            //    {
+
+            using (var sr = new StreamReader(@"Beispiel.csv"))
+            {
+                using (var sw = new StreamWriter(@"Beispieloutput.csv"))
+                {
+
+
+                    var reader = new CsvReader(sr);
+                    var writer = new CsvWriter(sw);
+                    var config = reader.Configuration;
+                    config.Delimiter = "\t";
+                    config.Encoding = Encoding.UTF8;
+                    config.HasHeaderRecord = true;
+                    reader.Configuration.RegisterClassMap<BeispielRecordMap>();
+
+                    //CSVReader will now read the whole file into an enumerable
+                    IEnumerable records = reader.GetRecords<BeispielRecord>().ToList();
+
+                    //Write the entire contents of the CSV file into another
+                    writer.WriteRecords(records);
+
+
+                    //Now we will write the data into the same output file but will do it 
+                    //Using two methods.  The first is writing the entire record.  The second
+                    //method writes individual fields.  Note you must call NextRecord method after 
+                    //using Writefield to terminate the record.
+
+                    //Note that WriteRecords will write a header record for you automatically.  If you 
+                    //are not using the WriteRecords method and you want to a header, you must call the 
+                    //Writeheader method like the following:
+                    //
+                    //writer.WriteHeader<DataRecord>();
+                    //
+                    //Do not use WriteHeader as WriteRecords will have done that already.
+
+                    foreach (DataRecord record in records)
+                    {
+                        //Write entire current record
+                        writer.WriteRecord(record);
+
+                        //write record field by field
+                        writer.WriteField(record.CommonName);
+                        writer.WriteField(record.FormalName);
+                        writer.WriteField(record.TelephoneCode);
+                        writer.WriteField(record.CountryCode);
+                        //ensure you write end of record when you are using WriteField method
+                        writer.NextRecord();
+                    }
+                }
+            }
+        }
+    }
+}
